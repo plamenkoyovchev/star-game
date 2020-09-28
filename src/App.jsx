a1e8cd373cac8aa553d7689372ec81e1cbc7f1e4
Plamen Yovchev <plamenkooo@gmail.com>
2020-09-28 12:39:44 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,57 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+import utils from "./shared/math-utils";
+
+const StarMatch = () => {
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+        </div>
+        <div className="right">
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+          <button className="number">4</button>
+          <button className="number">5</button>
+          <button className="number">6</button>
+          <button className="number">7</button>
+          <button className="number">8</button>
+          <button className="number">9</button>
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
+    </div>
+  );
+};
+
+// Color Theme
+const colors = {
+  available: "lightgray",
+  used: "lightgreen",
+  wrong: "lightcoral",
+  candidate: "deepskyblue",
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <StarMatch />
+    </div>
+  );
+};
 
 export default App;
