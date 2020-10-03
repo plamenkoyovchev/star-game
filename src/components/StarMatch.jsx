f9a4a24eb75bddd8443ec2135bfcb33f78caf5a9
plamenyovchev <plamenkooo@gmail.com>
2020-10-03 19:57:55 +0300

    use useGameState custom hook to manage game state

--- src/components/StarMatch.jsx
+++ src/components/StarMatch.jsx
@@ -1,24 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import useGameState from "../hooks/useGameState";
 import utils from "../shared/math-utils";
 
 import GameStatus from "./GameStatus";
 import PlayableNumber from "./PlayableNumber";
 
 const StarMatch = ({ startNewGame }) => {
-  const [stars, setStars] = useState(utils.random(1, 9));
-  const [availableNumbers, setAvailableNumbers] = useState(utils.range(1, 9));
-  const [candidates, setCandidates] = useState([]);
-  const [secondsLeft, setSecondsLeft] = useState(10);
-
-  useEffect(() => {
-    if (secondsLeft > 0 && availableNumbers.length > 0) {
-      const timerId = setTimeout(() => {
-        setSecondsLeft(secondsLeft - 1);
-      }, 1000);
-
-      return () => clearTimeout(timerId);
-    }
-  });
+  const {
+    stars,
+    availableNumbers,
+    candidates,
+    secondsLeft,
+    setGameState,
+  } = useGameState();
 
   const candidateIsWrong = utils.sum(candidates) > stars;
   const gameStatus =
@@ -45,22 +39,7 @@ const StarMatch = ({ startNewGame }) => {
       return;
     }
 
-    const newCandidates =
-      status === "available"
-        ? [...candidates, number]
-        : candidates.filter((cn) => cn !== number);
-
-    if (utils.sum(newCandidates) !== stars) {
-      setCandidates(newCandidates);
-    } else {
-      const newAvailableNumbers = availableNumbers.filter(
-        (an) => !newCandidates.includes(an)
-      );
-
-      setStars(utils.randomSumIn(newAvailableNumbers, 9));
-      setAvailableNumbers(newAvailableNumbers);
-      setCandidates([]);
-    }
+    setGameState(status, number);
   };
 
   return (
